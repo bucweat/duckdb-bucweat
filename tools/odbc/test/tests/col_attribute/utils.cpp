e73dba5af91aceec88f7a80dbed8748fc4cff126
bucweat <M0dulu5!>
2023-11-13 19:29:16 +0400

    added test files, added SQL macro value lookup table to utils.cpp

--- tools/odbc/test/tests/col_attribute/utils.cpp
+++ tools/odbc/test/tests/col_attribute/utils.cpp
@@ -20,6 +20,30 @@ void odbc_col_attribute_test::ExpectError(SQLHANDLE handle, SQLSMALLINT field_id
 	REQUIRE(ret == SQL_ERROR);
 }
 
+static const std::map<int, std::string> sql_lookup {
+	{SQL_DESC_CASE_SENSITIVE, "SQL_DESC_CASE_SENSITIVE"},
+	{SQL_DESC_CATALOG_NAME, "SQL_DESC_CATALOG_NAME"},
+	{SQL_DESC_CONCISE_TYPE, "SQL_DESC_CONCISE_TYPE"},
+	{SQL_DESC_COUNT, "SQL_DESC_COUNT"},
+	{SQL_DESC_DISPLAY_SIZE, "SQL_DESC_DISPLAY_SIZE"},
+	{SQL_DESC_FIXED_PREC_SCALE, "SQL_DESC_FIXED_PREC_SCALE"},
+	{SQL_DESC_LENGTH, "SQL_DESC_LENGTH"},
+	{SQL_DESC_LITERAL_PREFIX, "SQL_DESC_LITERAL_PREFIX"},
+	{SQL_DESC_LITERAL_SUFFIX, "SQL_DESC_LITERAL_SUFFIX"},
+	{SQL_DESC_LOCAL_TYPE_NAME, "SQL_DESC_LOCAL_TYPE_NAME"},
+	{SQL_DESC_NULLABLE, "SQL_DESC_NULLABLE"},
+	{SQL_DESC_NUM_PREC_RADIX, "SQL_DESC_NUM_PREC_RADIX"},
+	{SQL_DESC_PRECISION, "SQL_DESC_PRECISION"},
+	{SQL_COLUMN_SCALE, "SQL_COLUMN_SCALE"},
+	{SQL_DESC_SCALE, "SQL_DESC_SCALE"},
+	{SQL_DESC_SCHEMA_NAME, "SQL_DESC_SCHEMA_NAME"},
+	{SQL_DESC_SEARCHABLE, "SQL_DESC_SEARCHABLE"},
+	{SQL_DESC_TYPE, "SQL_DESC_TYPE"},
+	{SQL_DESC_UNNAMED, "SQL_DESC_UNNAMED"},
+	{SQL_DESC_UNSIGNED, "SQL_DESC_UNSIGNED"},
+	{SQL_DESC_UPDATABLE, "SQL_DESC_UPDATABLE"}
+};
+
 void odbc_col_attribute_test::TestAllFields(SQLHANDLE hstmt, std::map<SQLLEN, ExpectedResult> expected) {
 	SQLLEN n;
 	SQLRETURN ret = SQLColAttribute(hstmt, 1, SQL_DESC_AUTO_UNIQUE_VALUE, nullptr, 0, nullptr, &n);
@@ -27,6 +51,7 @@ void odbc_col_attribute_test::TestAllFields(SQLHANDLE hstmt, std::map<SQLLEN, Ex
 	REQUIRE(n == SQL_FALSE);
 
 	for (auto it = expected.begin(); it != expected.end(); ++it) {
+		INFO("value --> " << sql_lookup.find(it->first)->second);
 		if (it->second.is_int) {
 			if (it->second.n == SQL_NO_TOTAL && (it->first == SQL_COLUMN_SCALE || it->first == SQL_DESC_SCALE)) {
 				// SQL_NO_TOTAL -> Returns SQL_SUCCESS_WITH_INFO
